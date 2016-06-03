19d5a654a55721b16d7fed891e4192b0f566b380
nralbrecht <nralbrecht@web.de>
2016-06-03 22:10:57 +0200

    Refactor the code
    The tabs on open gives you a tab object witch isn't initialized. To fix
    that, a state change event handler gets added to the low level
    tabbrowser. The state change event only gets thrown if the tab get
    activated.

--- index.js
+++ index.js
@@ -1,52 +1,26 @@
-var tabs = require('sdk/tabs');
-var utils = require("sdk/tabs/utils");
-var { viewFor } = require("sdk/view/core");
-var { Hotkey } = require("sdk/hotkeys");
-var { Cu, Ci } = require('chrome');
-Cu.import("resource://gre/modules/XPCOMUtils.jsm");
+let tabs = require('sdk/tabs');
+let utils = require("sdk/tabs/utils");
+let { viewFor } = require("sdk/view/core");
 
-var lastActiveTab = null;
+let lastActiveTab = null;
 
 tabs.on('open', function onOpen(tab){
-  console.log([tab.id, tab.title, tab.url, tab.index, tab.window, tab.readyState]);
   if(lastActiveTab != null){
     if(lastActiveTab.isPinned){
       const lowLevelTab = viewFor(tab);
       const lowLevelBrowser = utils.getTabBrowserForTab(lowLevelTab);
 
-      lowLevelBrowser.addProgressListener({
-        QueryInterface: XPCOMUtils.generateQI(["nsIWebProgressListener", "nsISupportsWeakReference"]),
-        onStateChange: function(aWebProgress, aRequest, aFlag, aStatus) {
-          // lowLevelBrowser.moveTabTo(lowLevelTab, tab.window.tabs.length + 1);
-          console.log("state");
-          if (aFlag & Ci.nsIWebProgressListener.STATE_START) {
-            // This fires when the load event is initiated
-            console.log("start");
-            // console.log({"flag": aFlag, "status": aStatus, "tab-readystate": tab.readyState});
-          }
-        },
-        onLocationChange: function(aProgress, aRequest, aURI, aFlag) {
-          console.log("location");
+      const progressEventHandler = {
+        onStateChange: function() {
           lowLevelBrowser.moveTabTo(lowLevelTab, tab.window.tabs.length + 1);
-        },
-        onProgressChange: function(aWebProgress, aRequest, curSelf, maxSelf, curTot, maxTot) {
-          console.log("progress");
-          // lowLevelBrowser.moveTabTo(lowLevelTab, tab.window.tabs.length + 1);
-        },
-        onStatusChange: function(aWebProgress, aRequest, aStatus, aMessage) {
-          console.log("status");
-          // lowLevelBrowser.moveTabTo(lowLevelTab, tab.window.tabs.length + 1);
-        },
-        onSecurityChange: function(aWebProgress, aRequest, aState) {
-          console.log("security");
-          // lowLevelBrowser.moveTabTo(lowLevelTab, tab.window.tabs.length + 1);
+          lowLevelBrowser.removeProgressListener(progressEventHandler);
         }
-      });
+      }
 
-      var tempTab = lastActiveTab;
+      lowLevelBrowser.addProgressListener(progressEventHandler);
 
+      let tempTab = lastActiveTab;
       tab.activate();
-      tab.index = tab.window.tabs.length + 1;
       tempTab.activate();
     }
   }
@@ -54,16 +28,4 @@ tabs.on('open', function onOpen(tab){
 
 tabs.on('activate', function onActive(tab){
   lastActiveTab = tab;
-});
-
-tabs.open({
-  url: "https://developer.mozilla.org/en-US/Add-ons/SDK/High-Level_APIs/tabs",
-  isPinned: true
-});
-
-var showHotKey = Hotkey({
-  combo: "accel-space",
-  onPress: function() {
-    var ij = lastActiveTab.window.tabs.length;
-  }
 });
