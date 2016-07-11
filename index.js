ed83f67b178133dd5758bc48e051902333a9d1d2
nralbrecht <nralbrecht@web.de>
2016-07-11 12:49:38 +0200

    Add compatibility for multiple windows
    Before, only one last tab was tracked over all opend windows. Now each window is handled on its own, also only the index of the last tab gets saved instead of the whole tab.

--- index.js
+++ index.js
@@ -1,34 +1,40 @@
-let tabs = require('sdk/tabs');
 let utils = require("sdk/tabs/utils");
 let options = require("sdk/simple-prefs");
 let { viewFor } = require("sdk/view/core");
+let windows = require('sdk/windows').browserWindows;
 
-let lastActiveTab = null;
+function onWindowOpen(window) {
+  let lastActiveTabIndex = window.tabs.activeTab.index;
 
-tabs.on('open', function onOpen(tab){
-  if(lastActiveTab != null){
-    if(lastActiveTab.isPinned){
+  window.tabs.on('activate', function onActive(tab){
+    lastActiveTabIndex = tab.index;
+  });
+
+  window.tabs.on('open', function onOpen(tab){
+    if(window.tabs[lastActiveTabIndex].isPinned){
       const lowLevelTab = viewFor(tab);
       const lowLevelBrowser = utils.getTabBrowserForTab(lowLevelTab);
 
       const progressEventHandler = {
         onStateChange: function() {
-          lowLevelBrowser.moveTabTo(lowLevelTab, tab.window.tabs.length + 1);
+          lowLevelBrowser.moveTabTo(lowLevelTab, window.tabs.length + 1);
           lowLevelBrowser.removeProgressListener(progressEventHandler);
         }
       }
 
       lowLevelBrowser.addProgressListener(progressEventHandler);
 
-      let tempTab = lastActiveTab;
+      let tempTabIndex = lastActiveTabIndex;
       tab.activate();
       if (!(options.prefs.openInForground || lowLevelTab.getAttribute("label") === "Neuer Tab")) {
-        tempTab.activate();
+        window.tabs[tempTabIndex].activate();
       }
     }
-  }
-});
+  });
+}
 
-tabs.on('activate', function onActive(tab){
-  lastActiveTab = tab;
-});
+for each (let window in windows) {
+  onWindowOpen(window);
+}
+
+windows.on('open', onWindowOpen);
